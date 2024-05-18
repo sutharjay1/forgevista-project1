c6a37171270fe67a9a6922896ee85c0ed609a6b7
Jay Suthar <suthajay3635@gmail.com>
2024-05-18 21:39:43 +0530

    Team Member component

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -24,6 +24,7 @@ import {
 	CarouselPrevious,
 } from '@/components/ui/carousel';
 import TeamCard from '../components/TeamCard';
+import EventCard from '../components/EventCard';
 
 const Info = ({ title, description }) => {
 	return (
@@ -269,6 +270,93 @@ const Home = () => {
 
 				{/*  */}
 			</div>
+			{/*  */}
+			<div className="w-full max-w-7xl mx-auto pt-14 pb-16">
+				<div className="w-full flex items-center justify-start gap-5 mb-4 lg:mb-8">
+					<span className="text-2xl md:text-3xl font-semibold text-[#16191E]">
+						Upcoming Events
+					</span>
+				</div>
+				<Carousel
+					orientation="horizontal"
+					opts={{
+						align: 'start',
+						loop: true,
+					}}
+				>
+					<CarouselContent className="	py-5">
+						<CarouselItem className="px-5 basic-0  md:basis-1/2 lg:basis-1/3">
+							<EventCard
+								eventTitle="Blood Donation Camp"
+								eventDescription="Blood Donation Camp"
+								eventImg="https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								eventDate="May 25, 2024"
+								eventTags={['Blood Donation']}
+								eventLocation="Location"
+								totalLiveParticipants={'10K'}
+								eventLink="/book/blood-donation"
+								eventTime="10:00 AM - 11:00 AM"
+								eventPrice="Free"
+								eventOrganizer="SombaHelp Foundation"
+								eventPurchaseLink="/book/blood-donation"
+								isLiked={true}
+							/>
+						</CarouselItem>
+						<CarouselItem className="px-5 basic-0  md:basis-1/2 lg:basis-1/3">
+							<EventCard
+								eventTitle="Tree Plantation"
+								eventDescription="Tree Plantation"
+								eventImg="https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								eventDate="May 29, 2024"
+								eventTags={['Tree Planting']}
+								eventLocation="Location"
+								totalLiveParticipants={'10K'}
+								eventLink="/book/tree-plantation"
+								eventTime="10:00 AM - 11:00 AM"
+								eventPrice="Free"
+								eventOrganizer="ABC Foundation"
+								eventPurchaseLink="ht/book/tree-plantation"
+								isLiked={true}
+							/>
+						</CarouselItem>
+						<CarouselItem className="px-5 basic-0  md:basis-1/2 lg:basis-1/3">
+							<EventCard
+								eventTitle="Road Cleaning"
+								eventDescription="Road Cleaning"
+								eventImg="https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								eventDate="May 29, 2024"
+								eventTags={['Road Cleaning']}
+								eventLocation="Location"
+								totalLiveParticipants={'10K'}
+								eventLink="/book/road-cleaning"
+								eventTime="10:00 AM - 11:00 AM"
+								eventPrice="Free"
+								eventOrganizer="ABC Foundation"
+								eventPurchaseLink="/book/road-cleaning"
+								isLiked={true}
+							/>
+						</CarouselItem>
+						<CarouselItem className="px-5 basic-0  md:basis-1/2 lg:basis-1/3">
+							<EventCard
+								eventTitle="Teaching Camp"
+								eventDescription="Teaching Camp"
+								eventImg="https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								eventDate="May 29, 2024"
+								eventTags={['Teaching Camp']}
+								eventLocation="Location"
+								totalLiveParticipants={'10K'}
+								eventLink="/book/teaching-camp"
+								eventTime="10:00 AM - 11:00 AM"
+								eventPrice="Free"
+								eventOrganizer="ABC Foundation"
+								eventPurchaseLink="/book/teaching-camp"
+								isLiked={false}
+							/>
+						</CarouselItem>
+					</CarouselContent>
+				</Carousel>
+			</div>
+			{/*  */}
 			<div className="max-w-7xl mx-auto px-5 pt-6 flex flex-col items-start justify-start selection:bg-[#0a755862]">
 				<div className="w-full flex flex-col items-center justify-center mt-8">
 					<div className="w-full flex flex-col items-center justify-start">
